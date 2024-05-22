5a8ead1b6ee10fb595bf14286d614e67ee007fe1
Andreas Dinauer <andreas.j.dinauer@gmail.com>
2024-05-22 18:10:16 +0200

    dev component removed

--- pages/account/listings/bearbeiten/[id].vue
+++ pages/account/listings/bearbeiten/[id].vue
@@ -3,24 +3,56 @@
         <h1>Bearbeiten</h1>
         <div v-if="listing" class="content">
             <Section heading="Allgemein">
-                <div class="col-2 tile">
-                    <UiInput label="Tierart">
-                        <select name="" id="">
-                            <option :value="key" v-for="[key, value] in animalTypes">{{ value }}</option>
-                        </select>
-                    </UiInput>
-                    <UiInput label="Status">
-                        <select name="" id="">
-                            <option value="ACTIVE">Aktiv</option>
-                            <option value="INACTIVE">Inaktiv</option>
-                            <option value="CONVEYED">Vermittelt</option>
-                        </select>
-                    </UiInput>
+                <div class="content">
+                    <div class="col-2 tile">
+                        <UiInput label="Tierart">
+                            <select name="" id="">
+                                <option :value="key" v-for="[key, value] in animalTypes">{{ value }}</option>
+                            </select>
+                        </UiInput>
+                        <UiInput label="Status">
+                            <select name="" id="">
+                                <option value="ACTIVE">Aktiv</option>
+                                <option value="INACTIVE">Inaktiv</option>
+                                <option value="CONVEYED">Vermittelt</option>
+                                <option value="CONVEYED">Teils vermittelt</option>
+                            </select>
+                        </UiInput>
+                    </div>
+                    <div class="center-center">
+                        <UiButton icon="save" reverse>Speichern</UiButton>
+                    </div>
                 </div>
             </Section>
-            <div class="center-center">
-                <UiButton icon="save" reverse>Speichern</UiButton>
-            </div>
+            <Section heading="Tiere">
+                <div class="col-2">
+                    <div class="content" v-for="animal in listing.animals">
+                        <div class="tile content">
+                            <UiInput label="Name">
+                                <input type="text" v-model="animal.name">
+                            </UiInput>
+                            <div class="col-3" v-if="animal.birthday">
+                                <UiInput label="Geburtsjahr">
+                                    <input type="number" v-model="animal.birthday.year">
+                                </UiInput>
+                                <UiInput label="Geburtsmonat">
+                                    <input type="number" v-model="animal.birthday.month">
+                                </UiInput>
+                                <UiInput label="Geburtstag">
+                                    <input type="number" v-model="animal.birthday.day">
+                                </UiInput>
+                            </div>
+                            <UiCheckbox label="Sterilisiert/Kastriert" :checked="true"></UiCheckbox>
+                            <UiInput label="Geschlecht">
+                                <UiRadio :code="key" :label="value" :active="animal.sex" v-for="[key, value] in sexes" @select="(sex: string | undefined) => animal.sex = sex"></UiRadio>
+                            </UiInput>
+                        </div>
+                        <div class="center-center">
+                            <UiButton icon="save" reverse>Speichern</UiButton>
+                        </div>
+                    </div>
+                </div>
+            </Section>
         </div>
         <LoadingComponent v-if="!listing"></LoadingComponent>
     </ScrollComponent>
@@ -30,6 +62,7 @@
 import { Listing } from '~/classes/Listing';
 import { getListingById } from '~/requests/listing';
 import { animalTypes } from '~/util/animal';
+import { sexes } from '~/util/animal/sex';
 
 const listing: Ref<Listing | undefined> = ref(undefined);
 
